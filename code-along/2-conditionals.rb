6147261309337c317d6f49636722ff974564a0f2
Jacob Martinez <jacob.martinez@kellogg.northwestern.edu>
2025-01-30 01:25:22 +0000

    3

--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -3,13 +3,68 @@
 # ruby 2-conditionals.rb
 
 # Booleans
+test_is_true=true
+puts test_is_true
+
+test_is_false = false
+puts test_is_false
 
 # Boolean Expressions
+puts nil
+puts 3==2
+puts 3!=2     #not equal
+puts 3>2
+puts 3<2
 
 # If Conditional Logic
+puts nil
+if 3==2
+    puts "this text should never be displayed"
+end
+
+if 3>2
+    puts "this text should always display"
+end
 
 # If/Else Conditional Logic
+puts """"
+if 3==2
+    puts "this text should never be displayed"
+else
+    puts "oops"
+end
+
 
 # Elsif Conditional Logic
+puts """"
+user_password = "tacos"
+real_password = "secret"
 
+if user_password == real_password
+    puts "right password"
+else 
+    puts "wrong password"
+end
+
+bank_bal = 51
+
+if bank_bal>50
+    bank_bal = bank_bal-50
+    puts "50 withdrawn"
+else
+    puts "insuffiecient funds"
+end
+
+puts "bank balance is: " + bank_bal.to_s
 # Combining Expressions
+puts """"
+
+temp = 68
+precip = 0
+
+if temp >=65 && temp <= 75 && precip ==0
+puts"It's perf outside"
+end
+
+# OR ||
+# AND &&
--- code-along/3-arrays.rb
+++ code-along/3-arrays.rb
@@ -2,10 +2,24 @@
 # is the same as where this file is located and then run:
 # ruby 3-arrays.rb
 
+favorite_foods = ["tacos", "pizza", "ice cream"]
+puts favorite_foods
+
+numberz = [4, 8, 15, 16, 23, 42]  # called we- ahve to go back in class
+puts numberz
+
+mixed_array = ["tacos", 12, true] #unique to ruby, may not be true for most languages 
+puts mixed_array
+
+shopping_lists = [["milk", "eggs", "toilet paper"], ["soap", "shampoo", "shaving cream", "toilet paper"]]
+puts shopping_lists
+
 # Arrays are lists of things
 
 # Accessing the array
-
+puts favorite_foods[0] #can use negative numbers to go backwards in an array 
+puts """"
+puts shopping_lists[1][2]
 # Add to the array
 
 # There are lots of fun things you can do with arrays:
