a65c7ef0106e462c1b210566f11dd451cdb4832e
Jacob Martinez <jacob.martinez@kellogg.northwestern.edu>
2025-01-30 00:43:40 +0000

    basics

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,15 +5,58 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
+puts 2
+puts 3
+
+puts 5+2
+puts 5-2
+puts 5*2
+puts 5/2.0
 
 # Non-numbers
+puts "Hello, world!"
+
+puts "tacos are" + " delish!"
+puts "tacos" * 3
+puts "tacos:" + 3.to_s
 
 # True, False
+puts true
+puts false 
+
 
 # Nothing
+puts nil
 
 # Variables
+x=10
+y=3
+
+food = "tacos"
+quantity = 3
+puts food*quantity
 
 # Combine strings and variables
+puts nil
+
+first_name = "boba"
+greeting = "Hello, #{first_name}"
+
+puts greeting
+
+puts "#{food}: #{quantity}"
 
 # String manipulation
+puts "hello".length
+
+# .length puts the length of the string
+# to comment and un commentout quickly can highligh block of text and hit "ctrl + /""
+
+puts "how are you today?".capitalize
+puts "computer".reverse
+
+creed = "This IS The Way"
+puts creed.upcase
+puts creed.downcase
+puts creed.swapcase
+# puts"\"\" "     -- use this to actually print quotes
